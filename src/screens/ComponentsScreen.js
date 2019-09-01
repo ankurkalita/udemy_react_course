2821b05ef578e55c61a581b07a61849576797216
Ankur <ankurkalitaofficial@gmail.com>
2019-09-01 19:44:42 +0530

    Section 2 , Lesson 14

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 const greetings = <Text>Bye There!!</Text>;
+const name = 'Ankur';
+
 const ComponentsScreen = () =>{
     return (
         <View>
-        <Text style={styles.textStyle}>Hello from Components Screen</Text>
-        {greetings}
+        <Text style={styles.textStyle}>Getting started with react native</Text>
+        <Text style = {styles.subheading}>Hello My name is {name}</Text>
         </View>
         );
 };
 
 const styles = StyleSheet.create({
    textStyle : {
-       fontSize : 30
+       fontSize : 45
+   },
+   subheading :{
+      fontSize  : 20
    }
 });
 
