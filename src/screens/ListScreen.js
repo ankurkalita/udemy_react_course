83d36763004e6ed7e5e9961cb534e4262a29217d
Ankur <ankurkalitaofficial@gmail.com>
2019-09-02 00:14:43 +0530

    Section 3 , Lesson 19

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () =>{
-    return <Text style = {styles.listFont}>ListScreen</Text>;
+    const friends = [
+        {'name':'friend #1'},
+        {'name':'friend #2'},
+        {'name':'friend #3'},
+        {'name':'friend #4'},
+        {'name':'friend #5'},
+        {'name':'friend #6'},
+        {'name':'friend #7'},
+        {'name':'friend #8'}
+    ];
+
+    return (
+    <FlatList 
+       data = {friends}
+       keyExtractor = {friend => friend.name }
+       renderItem = { ( {item} ) => {
+          return <Text>{item.name}</Text>;
+       }}
+    />);
 };
 
 const styles = StyleSheet.create({
